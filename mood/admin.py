03a6753a291b911df27adf100ee3edebd66af08d
Arthur Vilela <arthur.vilela@gmail.com>
2025-01-02 13:58:59 +0000

    Register 'mood' model in admin.py

--- mood/admin.py
+++ mood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Mood
 
 # Register your models here.
+
+@admin.register(Mood)
+class MoodAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'mood_type', 'note', 'action', 'created_at', 'updated_at')
+    list_filter = ('mood_type', 'date', 'created_at')
+    search_fields = ('user__username', 'mood_type', 'note', 'action')
