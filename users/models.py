81c6caf0eefb8e573ab97e5639ab9414211757d3
Arthur Vilela <arthur.vilela@gmail.com>
2025-01-03 18:36:25 +0000

    Create models for 'users' app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)  # Email for notifications
+    username = models.CharField(max_length=150, unique=True)  # Username for login
+
+    def __str__(self):
+        return self.username
