911ac4087378f2f9699e4a7d19d46dea06e29b55
Arthur Vilela <arthur.vilela@gmail.com>
2025-01-07 19:37:14 +0000

    Update setting url path, add 'settings' button in navbar

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('mood-history/', views.mood_history_view, name='mood_history'),
     path("edit-mood/<int:pk>/", views.edit_mood_view, name="edit_mood"),
     path("delete-mood/<int:pk>/", views.delete_mood_view, name="delete_mood"),
-    path('notification-settings/', views.notification_settings_view, name='notification_settings'),
+    path('settings/', views.settings_view, name='settings'),
 ]
