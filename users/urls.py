19cd55db0c9dd5da26cf22b4cb4ded5856b47c20
Arthur Vilela <arthur.vilela@gmail.com>
2025-01-08 20:02:31 +0000

    Add password change/profile update views and templates

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import logout_confirm_view
+from .views import logout_confirm_view, ProfileUpdateView, CustomPasswordChangeView
 
 app_name = "users"
 
 urlpatterns = [
     path("logout/", logout_confirm_view, name="logout_confirm"),
+    path("edit-profile/", ProfileUpdateView.as_view(), name="edit_profile"),
+    path("change-password/", CustomPasswordChangeView.as_view(), name="change_password"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import logout
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import UpdateView
+from .forms import ProfileUpdateForm
+from .forms import StyledPasswordChangeForm
 
 # Create your views here.
 
@@ -10,3 +18,20 @@ def logout_confirm_view(request):
         logout(request)
         return redirect("/")
     return render(request, "account/logout_confirm.html")
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdateView(UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = "users/profile_update.html"
+    success_url = "/dashboard/"  # Replace with your desired redirect URL
+
+    def get_object(self):
+        return self.request.user
+
+@method_decorator(login_required, name='dispatch')
+class CustomPasswordChangeView(PasswordChangeView):
+    form_class = StyledPasswordChangeForm
+    template_name = "users/password_change.html"
+    success_url = reverse_lazy("dashboard:dashboard_home")  # Adjust this to your dashboard's name
