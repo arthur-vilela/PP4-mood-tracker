3f914935fc2c67ccf0ed3e5fe435f1c4c5d9d9c1
Arthur Vilela <arthur.vilela@gmail.com>
2025-01-12 22:00:55 +0000

    Change email reminder text and add link to homepage

--- mood/management/commands/send_reminders.py
+++ mood/management/commands/send_reminders.py
@@ -1,6 +1,7 @@
 import os
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from mood.models import NotificationSettings
 
 class Command(BaseCommand):
@@ -8,16 +9,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         users = NotificationSettings.objects.filter(notify_by_email=True)
+        homepage_url = "https://pp4-mood-tracker-20082cf10f44.herokuapp.com/"
+
         for user_settings in users:
             user = user_settings.user
             try:
+                # Create personalized email content
+                subject = "Mood Tracker Daily Reminder"
+                plain_message = (
+                    "Hi there!\n\n"
+                    "This is your daily reminder to log your mood in the Mood Tracker app!\n"
+                    f"Click here to visit the app: {homepage_url}\n\n"
+                    "Stay consistent, and have a great day!"
+                )
+                html_message = render_to_string("emails/daily_reminder.html", {
+                    "user": user,
+                    "homepage_url": homepage_url,
+                })
+
+                # Send email
                 send_mail(
-                    subject="Mood Tracker Reminder",
-                    message="This is your daily reminder to log your mood in the Mood Tracker app!",
+                    subject=subject,
+                    message=plain_message,
                     from_email=os.getenv("EMAIL_USER"),
                     recipient_list=[user.email],
                     fail_silently=False,
+                    html_message=html_message,  # Include the HTML message
                 )
+                self.stdout.write(f"Email sent to {user.email}")
             except Exception as e:
                 self.stderr.write(f"Failed to send email to {user.email}: {e}")
-
